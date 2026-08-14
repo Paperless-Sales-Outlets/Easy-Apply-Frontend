d3fdf24fbc12baf1c2b05540a2ef20e55554887c
AvaneeshKesavan <kesavan.avaneesh@gmail.com>
2026-08-14 21:21:28 +0530

    fix: update PageWrapper usage in AnimatedRoutes to enable form styling for wizards

--- src/App.jsx
+++ src/App.jsx
@@ -151,7 +151,7 @@ const AnimatedRoutes = () => {
         <Route
           path="/new-connection"
           element={
-            <PageWrapper>
+            <PageWrapper form>
               <OtpProtectedForm>
                 <NewConnectionWizard />
               </OtpProtectedForm>
@@ -162,7 +162,7 @@ const AnimatedRoutes = () => {
         <Route
           path="/reconnection"
           element={
-            <PageWrapper>
+            <PageWrapper form>
               <OtpProtectedForm>
                 <ReconnectionWizard />
               </OtpProtectedForm>
@@ -173,7 +173,7 @@ const AnimatedRoutes = () => {
         <Route
           path="/ownership-change"
           element={
-            <PageWrapper>
+            <PageWrapper form>
               <OtpProtectedForm>
                 <OwnershipChangeWizard />
               </OtpProtectedForm>
@@ -184,7 +184,7 @@ const AnimatedRoutes = () => {
         <Route
           path="/location-change"
           element={
-            <PageWrapper>
+            <PageWrapper form>
               <OtpProtectedForm>
                 <LocationChangeWizard />
               </OtpProtectedForm>
@@ -195,7 +195,7 @@ const AnimatedRoutes = () => {
         <Route
           path="/termination"
           element={
-            <PageWrapper>
+            <PageWrapper form>
               <OtpProtectedForm>
                 <TerminationWizard />
               </OtpProtectedForm>
@@ -206,7 +206,7 @@ const AnimatedRoutes = () => {
         <Route
           path="/package-migration"
           element={
-            <PageWrapper>
+            <PageWrapper form>
               <OtpProtectedForm>
                 <PackageMigrationWizard />
               </OtpProtectedForm>
@@ -217,7 +217,7 @@ const AnimatedRoutes = () => {
         <Route
           path="/service-vacation"
           element={
-            <PageWrapper>
+            <PageWrapper form>
               <OtpProtectedForm>
                 <ServiceVacationWizard />
               </OtpProtectedForm>
@@ -228,7 +228,7 @@ const AnimatedRoutes = () => {
         <Route
           path="/refund-request"
           element={
-            <PageWrapper>
+            <PageWrapper form>
               <OtpProtectedForm>
                 <RefundRequestWizard />
               </OtpProtectedForm>
@@ -239,7 +239,7 @@ const AnimatedRoutes = () => {
         <Route
           path="/customer-request-acceptance"
           element={
-            <PageWrapper>
+            <PageWrapper form>
               <OtpProtectedForm>
                 <CustomerRequestAcceptanceWizard />
               </OtpProtectedForm>
@@ -250,7 +250,7 @@ const AnimatedRoutes = () => {
         <Route
           path="/internet-services"
           element={
-            <PageWrapper>
+            <PageWrapper form>
               <OtpProtectedForm>
                 <InternetServicesWizard />
               </OtpProtectedForm>
